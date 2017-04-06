d4253d306fa7d278cbe73b5784e4134216c5222b
Yang Ke <saint_yk@163.com>
2017-04-06 22:36:12 +0800

    style note item

--- src/js/components/Note.js
+++ src/js/components/Note.js
@@ -5,8 +5,8 @@ var AppStore = require('../stores/AppStore');
 var Note = React.createClass({
 	render: function() {
 		return (
-			<div>
-			  A Note
+			<div className="column">
+			  	<div className="note"><p>{this.props.note.text}</p></div>
 			</div>
 		);
 	}
